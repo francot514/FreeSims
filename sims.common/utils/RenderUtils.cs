6865f487a0704401dc5b39db350abae4cd2950e5
Franco Franks <francotf514@gmail.com>
2017-01-26 16:20:46 -0500

    Update RenderUtils.cs

--- sims.common/utils/RenderUtils.cs
+++ sims.common/utils/RenderUtils.cs
@@ -20,45 +20,135 @@ namespace TSO.Common.utils
 {
     public class RenderUtils
     {
-        public static RenderTarget2D CreateRenderTarget(GraphicsDevice device, int numberLevels, int multisample, SurfaceFormat surface, int width, int height)
+        
+        private static GraphicsDevice GD;
+        private static RenderTarget2D BackbufferDepth;
+        private static RenderTarget2D Backbuffer;
+        private static SpriteBatch SB;
+
+        public static void InitGD(GraphicsDevice gd)
         {
-            //apparently in xna4, there is no way to check device format... (it looks for the closest format if desired is not supported) need to look into if this affects anything.
+            GD = gd;
+            SB = new SpriteBatch(gd);
+        }
+        public static void InitScreenTargets(GraphicsDevice gd)
+        {
+            if (BackbufferDepth != null) BackbufferDepth.Dispose();
+            if (Backbuffer != null) Backbuffer.Dispose();
+            var scale = 1;
+            BackbufferDepth = CreateRenderTarget(gd, 1, 0, SurfaceFormat.Color, gd.Viewport.Width / scale, gd.Viewport.Height / scale, DepthFormat.None);
+            Backbuffer = CreateRenderTarget(gd, 1, 0, SurfaceFormat.Color, gd.Viewport.Width / scale, gd.Viewport.Height / scale, DepthFormat.Depth24Stencil8);
+        }
 
-            /*MultiSampleType type = device.PresentationParameters.MultiSampleType;
+        private static RenderTarget2D ActiveColor;
+        private static RenderTarget2D ActiveDepth;
+        private static int StencilValue;
 
-            // If the card can't use the surface format
-            if (!GraphicsAdapter.DefaultAdapter.CheckDeviceFormat(
-                DeviceType.Hardware,
-                GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Format,
-                TextureUsage.None,
-                QueryUsages.None,
-                ResourceType.RenderTarget,
-                surface))
+        public static void SetPPXTarget(RenderTarget2D color, RenderTarget2D depth, bool clear)
+        {
+            if (color == null && depth == null && Backbuffer != null) color = Backbuffer;
+            ActiveColor = color;
+            if (color == Backbuffer && depth == null && BackbufferDepth != null) depth = BackbufferDepth;
+            ActiveDepth = depth;
+
+            var gd = GD;
+            gd.SetRenderTarget(color); //can be null
+            if (clear)
             {
-                // Fall back to current display format
-                surface = device.DisplayMode.Format;
+                StencilValue = 1;
+                gd.Clear(Color.TransparentBlack);
+                if (depth != null)
+                {
+                    gd.SetRenderTarget(depth);
+                    gd.Clear(Color.White);
+                }
             }
-            // Or it can't accept that surface format 
-            // with the current AA settings
-            else if (!GraphicsAdapter.DefaultAdapter.CheckDeviceMultiSampleType(
-                DeviceType.Hardware, surface,
-                device.PresentationParameters.IsFullScreen, type))
+            
+            if (depth != null) gd.SetRenderTargets(color, depth);
+           
+        }
+
+        public delegate void RenderPPXProcedureDelegate(bool depthPass);
+        public static void RenderPPXDepth(Effect effect, bool forceDepth,
+            RenderPPXProcedureDelegate proc)
+        {
+            var color = ActiveColor;
+            var depth = ActiveDepth;
+            var gd = GD;
+            if (depth != null)
             {
-                // Fall back to no antialiasing
-                type = MultiSampleType.None;
-            }*/
+                var oldDS = gd.DepthStencilState;
+                //completely special case.
+                gd.SetRenderTarget(color);
+                gd.DepthStencilState = new DepthStencilState
+                {
+                    StencilEnable = true,
+                    StencilFunction = CompareFunction.Always,
+                    StencilFail = StencilOperation.Keep,
+                    StencilPass = StencilOperation.Replace,
+                    CounterClockwiseStencilPass = StencilOperation.Replace,
+                    StencilDepthBufferFail = StencilOperation.Keep,
+                    DepthBufferEnable = forceDepth, //(ActiveColor == null),
+                    DepthBufferWriteEnable = forceDepth, //(ActiveColor == null),
+                    ReferenceStencil = StencilValue,
+                    TwoSidedStencilMode = true
+                };
+                effect.Parameters["depthMap"].SetValue(depth);
+                effect.Parameters["depthOutMode"].SetValue(false);
+                proc(false);
 
-            /*int width, height;
+                //now draw the depth using the depth test information we got previously.
 
-            // See if we can use our buffer size as our texture
-            CheckTextureSize(device.PresentationParameters.BackBufferWidth,
-                device.PresentationParameters.BackBufferHeight,
-                out width, out height);*/
+                //unbind depth map since we are writing to it
+                effect.Parameters["depthMap"].SetValue((Texture2D)null);
+                effect.Parameters["depthOutMode"].SetValue(true);
+                gd.SetRenderTarget(depth);
+                gd.DepthStencilState = new DepthStencilState
+                {
+                    StencilEnable = true,
+                    StencilFunction = CompareFunction.Equal,
+                    DepthBufferEnable = forceDepth,
+                    DepthBufferWriteEnable = forceDepth,
+                    ReferenceStencil = StencilValue,
+                };
+                proc(true);
 
+                gd.DepthStencilState = oldDS;
+                StencilValue++; //can increment up to 254 times. Assume we're not going to be rendering that much between clears.
+                if (StencilValue > 255) StencilValue = 1;
+                gd.SetRenderTarget(color);
+                effect.Parameters["depthOutMode"].SetValue(false);
+            }
+            else if (depth != null)
+            {
+                //draw color then draw depth
+                gd.SetRenderTarget(color);
+                proc(false);
+                gd.SetRenderTarget(depth);
+                proc(true);
+            }
+            else
+            {
+                //mrt already bound. draw in both.
+                proc(false);
+            }
+        }
+
+        public static void DrawBackbuffer()
+        {
+            if (Backbuffer == null) return; //this gfx mode does not use a rendertarget backbuffer
+            SB.Begin();
+            SB.Draw(Backbuffer, new Vector2(), null, Color.White, 0f, new Vector2(), new Vector2(1, 1),
+                SpriteEffects.None, 0);
+            SB.End();
+        }
+        
+        public static RenderTarget2D CreateRenderTarget(GraphicsDevice device, int numberLevels, int multisample, SurfaceFormat surface, int width, int height,  DepthFormat dformat)
+        {
             // Create our render target
             return new RenderTarget2D(device,
                 width, height, (numberLevels>1), surface,
-                DepthFormat.Depth24, 0, RenderTargetUsage.PreserveContents);
+                dformat, 0, RenderTargetUsage.PreserveContents);
         }
     }
 }
