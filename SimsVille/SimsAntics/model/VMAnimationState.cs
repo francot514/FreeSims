925adbd704a90bc63838492201e12f4335c5b05b
Franco Franks <francotf514@gmail.com>
2017-05-17 16:57:22 -0500

    Updated sims antics

---
+++ SimsVille/SimsAntics/model/VMAnimationState.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/. 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TSO.Vitaboy;
+using TSO.SimsAntics.Utils;
+
+namespace TSO.SimsAntics.Model
+{
+    public class VMAnimationState {
+        public Animation Anim;
+        public float CurrentFrame;
+        public short EventCode;
+        public bool EventFired;
+        public bool EndReached;
+        public bool PlayingBackwards;
+        public float Speed = 1.0f;
+        public float Weight = 1.0f; //For animation blending. All active animations should add up to 1 but won't break if it doesn't.
+        public bool Loop = false;
+        public List<TimePropertyListItem> TimePropertyLists = new List<TimePropertyListItem>();
+
+        public VMAnimationState(Animation animation, bool backwards)
+        {
+            Anim = animation;
+
+            if (backwards)
+            {
+                PlayingBackwards = true;
+                CurrentFrame = Anim.NumFrames;
+            }
+
+            foreach (var motion in animation.Motions)
+            {
+                if (motion.TimeProperties == null) { continue; }
+
+                foreach (var tp in motion.TimeProperties)
+                {
+                    foreach (var item in tp.Items)
+                    {
+                        TimePropertyLists.Add(item);
+                    }
+                }
+            }
+
+            /** Sort time property lists by time **/
+            TimePropertyLists.Sort(new TimePropertyListItemSorter());
+        }
+
+
+    }
+}
