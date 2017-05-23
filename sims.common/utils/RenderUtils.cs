dbb71e9d98900661135dddca9d256b8a85be44b9
Franco Franks <francotf514@gmail.com>
2017-05-23 17:32:49 -0500

    Update renderutils

--- sims.common/utils/RenderUtils.cs
+++ sims.common/utils/RenderUtils.cs
@@ -14,6 +14,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace TSO.Common.utils
