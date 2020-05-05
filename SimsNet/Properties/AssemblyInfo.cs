5d68c4512da642da7583fc4a5e8acd949a6064f1
Franco <francotf514@gmail.com>
2020-05-05 18:19:47 -0500

    Quick projects update

--- SimsNet/Properties/AssemblyInfo.cs
+++ SimsNet/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimsNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("FreeSims Multiplayer server")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SimsNet")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
