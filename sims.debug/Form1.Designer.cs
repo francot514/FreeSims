aa7187ad2a58614bdaabcc3b4aae5bf46a6ac0c5
Franco Franks <francotf514@gmail.com>
2023-03-09 17:03:24 -0500

    Quick debug fix

--- sims.debug/Form1.Designer.cs
+++ sims.debug/Form1.Designer.cs
@@ -162,6 +162,7 @@
             this.button4.TabIndex = 18;
             this.button4.Text = "Roofs";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
