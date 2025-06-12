c7b70b48f18dbad7538a84b7f4284cc3e09f78b7
Franco Franks <francotf514@gmail.com>
2025-06-12 18:52:49 -0500

    Add dynamic tuning

---
+++ sims.common/Model/DynTuningEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace FSO.Common.Model
+{
+    public class DynTuningEntry
+    {
+        public string tuning_type { get; set; }
+        public int tuning_table { get; set; }
+        public int tuning_index { get; set; }
+        public float value { get; set; }
+    }
+}
