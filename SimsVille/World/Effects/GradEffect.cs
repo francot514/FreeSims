6172a0027d4c5cc03573647908e02c926ce3d5a6
Franco Franks <francotf514@gmail.com>
2025-10-12 19:19:16 -0500

    Add new effects

---
+++ SimsVille/World/Effects/GradEffect.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FSO.LotView.Effects
+{
+    /// <summary>
+    /// An effect for drawing gradients. Used to draw objcet/wall shadow geometry for Moderate FSO Lighting
+    /// </summary>
+    public class GradEffect : WorldEffect
+    {
+        protected override Type TechniqueType => base.TechniqueType;
+
+        private EffectParameter pProjection;
+        
+        public Matrix Projection
+        {
+            set
+            {
+                pProjection.SetValue(value);
+            }
+        }
+
+        public GradEffect(GraphicsDevice graphicsDevice, byte[] effectCode) : base(graphicsDevice, effectCode)
+        {
+        }
+
+        public GradEffect(GraphicsDevice graphicsDevice, byte[] effectCode, int index, int count) : base(graphicsDevice, effectCode, index, count)
+        {
+        }
+
+        public GradEffect(Effect cloneSource) : base(cloneSource)
+        {
+        }
+
+        protected override void PrepareParams()
+        {
+            base.PrepareParams();
+
+            pProjection = Parameters["Projection"];
+        }
+    }
+
+    public enum GradEffectTechniques
+    {
+        Draw2D
+    }
+}
