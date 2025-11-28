a26b9a54f6f6e6c361647ffc983952069d29e57e
Franco Franks <francotf514@gmail.com>
2025-11-27 19:45:38 -0500

    Little update

--- SimsNet/Properties/AssemblyInfo.cs
+++ SimsNet/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SimsNet")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
