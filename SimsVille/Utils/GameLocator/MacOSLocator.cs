6d3a116fec44a24472151d722a7d55a3b49a78e6
Franco Franks <francotf514@gmail.com>
2026-04-15 17:39:16 -0500

    Create MacOSLocator.cs

---
+++ SimsVille/Utils/GameLocator/MacOSLocator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace FSO.Client.Utils.GameLocator
+{
+    public class MacOSLocator : ILocator
+    {
+        public string FindTheSimsOnline()
+        {
+            string localDir = @"../The Sims Online/TSOClient/";
+            if (File.Exists(Path.Combine(localDir, "tuning.dat"))) return localDir;
+            
+            return string.Format("{0}/Documents/The Sims Online/TSOClient/", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+        }
+    }
+}
